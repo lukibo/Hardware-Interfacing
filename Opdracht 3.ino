31e26ad12336686983c3833317dd1007cc405d58
lukibo <74361673+lukibo@users.noreply.github.com>
2022-09-15 13:12:20 +0200

    Add files via upload

---
+++ Opdracht 3.ino
@@ -0,0 +1,114 @@
+//Bron https://www.youtube.com/watch?v=JghpLbCtnUU
+
+int potPin = A5;
+int ledPin2 = 2;
+int ledPin3 = 3;
+int ledPin4 = 4;
+int ledPin5 = 5;
+int ledPin6 = 6;
+int ledPin7 = 7;
+int ledPin8 = 8;
+
+void setup() {
+  pinMode(potPin, INPUT);
+  pinMode(ledPin2, INPUT);
+  pinMode(ledPin3, INPUT);
+  pinMode(ledPin4, INPUT);
+  pinMode(ledPin5, INPUT);
+  pinMode(ledPin6, INPUT);
+  pinMode(ledPin7, INPUT);
+  pinMode(ledPin8, INPUT);
+}
+
+void loop() {
+  int potMeasure = analogRead(A5);
+  //alle leds uit
+  if(potMeasure < 30)
+  {
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin4, LOW);
+    digitalWrite(ledPin5, LOW);
+    digitalWrite(ledPin6, LOW);
+    digitalWrite(ledPin7, LOW);
+    digitalWrite(ledPin8, LOW);
+  }
+  //eerst led aan
+  else if(potMeasure < 147)
+  {
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin4, LOW);
+    digitalWrite(ledPin5, LOW);
+    digitalWrite(ledPin6, LOW);
+    digitalWrite(ledPin7, LOW);
+    digitalWrite(ledPin8, LOW);
+  }
+  //tweede led aan
+  else if (potMeasure < 294)
+  {
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, LOW);
+    digitalWrite(ledPin5, LOW);
+    digitalWrite(ledPin6, LOW);
+    digitalWrite(ledPin7, LOW);
+    digitalWrite(ledPin8, LOW);
+  }
+  //derde led aan
+  else if (potMeasure < 441)
+  {
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, LOW);
+    digitalWrite(ledPin6, LOW);
+    digitalWrite(ledPin7, LOW);
+    digitalWrite(ledPin8, LOW);
+  }
+  //vierde led aan
+  else if (potMeasure < 588)
+  {
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, HIGH);
+    digitalWrite(ledPin6, LOW);
+    digitalWrite(ledPin7, LOW);
+    digitalWrite(ledPin8, LOW);
+  }
+  //vijfde led aan
+  else if (potMeasure < 735)
+  {
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, HIGH);
+    digitalWrite(ledPin6, HIGH);
+    digitalWrite(ledPin7, LOW);
+    digitalWrite(ledPin8, LOW);
+  }
+  //zesde led aan
+  else if (potMeasure < 882)
+  {
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, HIGH);
+    digitalWrite(ledPin6, HIGH);
+    digitalWrite(ledPin7, HIGH);
+    digitalWrite(ledPin8, LOW);
+  }
+  //zevende led aan
+  else if (potMeasure < 1029)
+  {
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, HIGH);
+    digitalWrite(ledPin6, HIGH);
+    digitalWrite(ledPin7, HIGH);
+    digitalWrite(ledPin8, HIGH);
+  }
+
+}
