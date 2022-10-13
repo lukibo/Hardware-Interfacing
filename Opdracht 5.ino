e3a9537a0281736d92f1cf139af96cde57370b81
lukibo <74361673+lukibo@users.noreply.github.com>
2022-10-13 11:10:05 +0200

    Add files via upload

--- Opdracht 5.ino
+++ Opdracht 5.ino
@@ -1,41 +1,30 @@
-const int led11 = 11;
-const int led10 = 10;
-const int led9 = 9;
+//Bron Eloi
+const int ledR = 11;
+const int ledG = 10;
+const int ledB = 9;
+//kan je gebruiken voor het sneller doorlopen van alle mogelijke kleuren
 const int boost = 0;
 
 void setup() {
-    pinMode(led11, OUTPUT);
-    pinMode(led10, OUTPUT);
-    pinMode(led9, OUTPUT);
-    //nodig voor het noteren van alle kleuren
-    Serial.begin(9600);
+    pinMode(ledR, OUTPUT);
+    pinMode(ledG, OUTPUT);
+    pinMode(ledB, OUTPUT);
 }
 
 void loop() {
-  //door lopen van alle mogelijke rode kleuren
-    for (int rValue = 0; rValue < 255; rValue++) {
-        analogWrite(led11, rValue);
-        rValue += boost;
-        Serial.print("Red");
-        Serial.print(rValue);
-        Serial.print("\n");
-        //door lopen van alle mogelijke groene kleuren
-        for (int gValue = 0; gValue < 255; gValue++) {
-            analogWrite(led10, gValue);
-            gValue += boost;
-            Serial.print("\t");
-            Serial.print("Green");
-            Serial.print(gValue);
-            Serial.print("\n \t \t");
-            //door lopen van alle mogelijke blauwe kleuren
-            for (int bValue = 0; bValue < 255; bValue++) {
-                analogWrite(led9, bValue);
-                bValue += boost;
-                Serial.print("Blue");
-                Serial.print(bValue);
-                Serial.print("\t");
+    //doorlopen van alle rode kleuren
+    for (int redValue = 0; redValue < 255; redValue++) {
+        analogWrite(ledR, redValue);
+      	redValue = redValue + boost;
+        //doorlopen van alle groene kleuren
+        for (int greenValue = 0; greenValue < 255; greenValue++) {
+            analogWrite(ledG, greenValue);
+            greenValue = greenValue + boost;
+            //doorlopen van alle blauwe kleuren
+            for (int blueValue = 0; blueValue < 255; blueValue++) {
+                analogWrite(ledB, blueValue);
+                blueValue = blueValue + boost;
             }
-            Serial.print("\n");
         }
     }
   delay(10000);
